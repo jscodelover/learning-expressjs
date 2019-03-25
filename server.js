79c1dc5f8d0ab94fba11789425be318dc11b98c7
jscodelover <manisha.4918@gmail.com>
2019-03-25 23:40:58 +0530

    feat: serve static files

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
+const app = express();
 const port = process.env.port || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on ${port} port.....`);
+app.get("/", (req, res) => {
+  res.send("Hello Dev !!!");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => console.log(`Server running on ${port} port.....`));
